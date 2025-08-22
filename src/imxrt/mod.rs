c4931ca6c079726669728690fc32698f11fa82c7
Wouter Geraedts <git@woutergeraedts.nl>
2025-08-22 15:54:48 +0200

    WIP first RC

--- src/imxrt/mod.rs
+++ src/imxrt/mod.rs
@@ -1,16 +1,24 @@
+use core::ops::Range;
+
 use ec_slimloader_descriptors::journal::flash::FlashJournal;
+use ec_slimloader_descriptors::AppImageDescriptor;
+use embassy_imxrt::flexspi::embedded_storage::FlexSpiNorStorage;
 use embassy_imxrt::flexspi::nor_flash::FlexSpiNorFlash;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embedded_storage_async::nor_flash::NorFlash;
 use partition_manager::{Partition, PartitionManager, RW};
 use static_cell::StaticCell;
 
-use crate::{imxrt::storage_async::AsyncWrapper, panic, Board};
+use crate::imxrt::storage_async::AsyncWrapper;
+use crate::{panic, warn, Board, BootError};
 
+mod bootload;
 mod fcb;
 mod rom;
 mod storage_async;
 
-const JOURNAL_BUFFER_SIZE: usize = 1024;
+const MAXIMUM_SLOT_SIZE: usize = 1024 * 1024;
+const ALLOWED_APP_RANGE: Range<*mut u32> = (0x0009_0000 as *mut u32)..0x018_0000 as *mut u32;
 
 // auto-generated version information from Cargo.toml
 #[cfg(feature = "imxrt")]
@@ -25,7 +33,36 @@ pub unsafe fn raw_copy_to_ram(from: *const u32, to: *mut u32, len_words: usize) 
     core::ptr::copy_nonoverlapping(from, to, len_words);
 }
 
-type ExternalStorage = AsyncWrapper<embassy_imxrt::flexspi::embedded_storage::FlexSpiNorStorage<'static, 2, 2, 4096>>;
+type ExternalStorage = AsyncWrapper<FlexSpiNorStorage<'static, 2, 2, 4096>>;
+
+partition_manager::macros::create_partition_map!(
+    name: ExternalStorageConfig,
+    map_name: ExternalStorageMap,
+    variant: "bootloader",
+    manifest: "src/imxrt/ext-flash.toml"
+);
+
+#[derive(Debug, PartialEq)]
+#[allow(clippy::upper_case_acronyms)]
+struct IVT {
+    pub image_len: usize,
+    pub target_ptr: *mut u32,
+}
+
+impl IVT {
+    pub unsafe fn read(image_ptr: *const u32) -> Self {
+        Self {
+            image_len: *image_ptr.byte_add(0x20) as usize,
+            target_ptr: *image_ptr.byte_add(0x34) as *mut u32,
+        }
+    }
+
+    pub fn target_end_ptr(&self) -> Option<*mut u32> {
+        (self.target_ptr as usize)
+            .checked_add(self.image_len)
+            .map(|ptr| ptr as *mut u32)
+    }
+}
 
 struct Imxrt {
     journal: FlashJournal<Partition<'static, ExternalStorage, RW>>,
@@ -40,9 +77,7 @@ impl Board for Imxrt {
             Err(e) => panic!("Failed to initialize FlexSPI peripheral: {:?}", e),
         };
 
-        let ext_flash = match unsafe {
-            embassy_imxrt::flexspi::embedded_storage::FlexSpiNorStorage::<2, 2, 4096>::new(ext_flash)
-        } {
+        let ext_flash = match unsafe { FlexSpiNorStorage::<2, 2, 4096>::new(ext_flash) } {
             Ok(ext_flash) => ext_flash,
             Err(e) => panic!("Failed to wrap FlexSPI flash in embedded_storage adaptor: {:?}", e),
         };
@@ -53,22 +88,82 @@ impl Board for Imxrt {
 
         let ExternalStorageMap { bl_state } = ext_flash_manager.map(ExternalStorageConfig::new());
 
-        let journal = match FlashJournal::new::<JOURNAL_BUFFER_SIZE>(bl_state).await {
+        let journal = match FlashJournal::new::<{ crate::JOURNAL_BUFFER_SIZE }>(bl_state).await {
             Ok(journal) => journal,
             Err(e) => panic!("Failed to initialize the flash state journal: {:?}", e),
         };
 
         Self { journal }
     }
+
+    fn journal(&mut self) -> &mut FlashJournal<impl NorFlash> {
+        &mut self.journal
+    }
+
+    async fn check_and_boot(&mut self, descriptor: &AppImageDescriptor) -> BootError {
+        // Copy the image to RAM from flash, and ensure that everything from flash is no longer available.
+        let ram_ivt = {
+            // Fetch image size, which in MBI is located in 0x20 of IVT.
+            let image_ptr = descriptor.slot_address as *const u32;
+            let slot_size = descriptor.slot_size_bytes as usize;
+
+            // Check if the image_len fits within the slot.
+            if slot_size > MAXIMUM_SLOT_SIZE {
+                return BootError::TooLarge;
+            }
+
+            let ivt = unsafe { IVT::read(image_ptr) };
+            if ivt.image_len > slot_size {
+                return BootError::TooLarge;
+            }
+
+            // Check if the target_ptr is within the allowed range.
+            // In MBI this is called the 'load_addr', which is located in 0x34 of IVT.
+            let image_target_end_ptr = match ivt.target_end_ptr() {
+                Some(ptr) => ptr,
+                None => return BootError::TooLarge,
+            };
+
+            if !ALLOWED_APP_RANGE.contains(&ivt.target_ptr) || !ALLOWED_APP_RANGE.contains(&image_target_end_ptr) {
+                return BootError::MemoryRegion;
+            }
+
+            unsafe {
+                raw_copy_to_ram(
+                    image_ptr,
+                    ivt.target_ptr,
+                    ivt.image_len.div_ceil(core::mem::size_of::<u32>()),
+                );
+            }
+
+            let ram_ivt = unsafe { IVT::read(ivt.target_ptr) };
+            if ivt != ram_ivt {
+                return BootError::ChangeAfterRead;
+            }
+
+            ram_ivt
+        };
+
+        // Call the ROM API to ensure that the image is signed and not broken or tampered with.
+        match rom::skboot_authenticate(ram_ivt.target_ptr, ram_ivt.image_len as u32) {
+            Ok(()) => {}
+            Err(e) => {
+                warn!("Failed to authenticate {:?}", e);
+                return BootError::Authenticate;
+            }
+        }
+
+        // Boot to application, and we do not return from this function.
+        unsafe { bootload::boot_application(ram_ivt.target_ptr) }
+    }
+
+    fn abort(&mut self) -> ! {
+        loop {
+            cortex_m::asm::wfi();
+        }
+    }
 }
 
 pub async fn init() -> impl Board {
     Imxrt::init().await
 }
-
-partition_manager::macros::create_partition_map!(
-    name: ExternalStorageConfig,
-    map_name: ExternalStorageMap,
-    variant: "bootloader",
-    manifest: "src/imxrt/ext-flash.toml"
-);
--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,96 @@
 
 #[cfg(feature = "defmt")]
 use defmt_rtt as _;
-
-use ec_slimloader_descriptors::AppImageDescriptor;
+use ec_slimloader_descriptors::journal::flash::FlashJournal;
+use ec_slimloader_descriptors::journal::state::{Slot, State, Status};
+use ec_slimloader_descriptors::{AppImageDescriptor, BootableRegionDescriptors};
 use embassy_executor::Spawner;
+use embedded_storage_async::nor_flash::NorFlash;
 use panic_probe as _;
 
-mod descriptors;
 #[cfg(feature = "imxrt")]
 mod imxrt;
 
 #[cfg(feature = "imxrt")]
-use imxrt::{init, raw_copy_to_ram};
+use imxrt::init;
 
-mod bootload;
+mod descriptors;
 mod log;
 
+/// Maximum buffer size on stack that is used by the bootloader.
+const JOURNAL_BUFFER_SIZE: usize = 1024;
+
+/// A board that can boot an application image.
+///
+/// Typically a board needs to support the intrinsics for some microcontroller and
+/// contain non volatile memory that stores the multiple images and bootloading state.
 trait Board {
+    /// Initialize the [Board], can only be called once.
     async fn init() -> Self;
+
+    /// Give a mutable reference to the [FlashJournal].
+    fn journal(&mut self) -> &mut FlashJournal<impl NorFlash>;
+
+    /// Check the application image for integrity, and try to boot.
+    ///
+    /// Does not return if the boot is successful.
+    /// Yields [BootError] if at any stage the boot is aborted.
+    async fn check_and_boot(&mut self, descriptor: &AppImageDescriptor) -> BootError;
+
+    /// Give up booting into an application.
+    ///
+    /// Either shut down the device or go into an infinite loop.
+    fn abort(&mut self) -> !;
 }
 
-#[cfg(not(feature = "imxrt"))]
-mod unsupported {
-    use super::*;
+#[derive(Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+enum BootError {
+    /// Image is too large to fit.
+    TooLarge,
+    /// Image requested to be copied into a disallowed memory region.
+    MemoryRegion,
+    /// What we copied from the NVM seems to have changed after initial read.
+    ///
+    /// Indicates a possible Man-in-the-Middle attack on the NVM.
+    ChangeAfterRead,
+    /// Image failed to authenticate.
+    Authenticate,
+    /// Failed to read the descriptor.
+    #[allow(unused)]
+    Descriptor(ec_slimloader_descriptors::ParseError),
+}
 
-    pub unsafe fn raw_copy_to_ram(_from: *const u32, _to: *mut u32, _len_words: usize) {}
-    pub fn validate_crc(_app_descriptor: &AppImageDescriptor) -> bool {
-        true
+impl From<ec_slimloader_descriptors::ParseError> for BootError {
+    fn from(value: ec_slimloader_descriptors::ParseError) -> Self {
+        BootError::Descriptor(value)
     }
 }
-use partition_manager::PartitionManager;
-#[cfg(not(feature = "imxrt"))]
-use unsupported::*;
 
-fn copy_image(_app_descriptor: &AppImageDescriptor) {
-    todo!()
-    // TODO allow other scenarios supported from bootloader aside from copy to RAM
-    // unsafe {
-    //     raw_copy_to_ram(
-    //         _app_descriptor.slot_address as *const u32,
-    //         _app_descriptor.execution_address as *mut u32,
-    //         _app_descriptor.execution_copy_size_bytes as usize / core::mem::size_of::<u32>(),
-    //     );
-    // }
+/// Intent which denotes which [Slot] should be booted.
+#[derive(Debug, PartialEq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+enum BootIntent {
+    Target,
+    Backup,
+}
+
+/// Attempt booting a specific slot.
+///
+/// Will not return if boot is successfull, and yield [BootError] in any other case.
+async fn attempt_slot(slot: Slot, board: &mut impl Board, descriptors: &BootableRegionDescriptors) -> BootError {
+    match descriptors.get_app_at_slot(u8::from(slot) as u32) {
+        Ok(active_app_descriptor) => board.check_and_boot(&active_app_descriptor).await,
+        Err(e) => e.into(),
+    }
+}
+
+/// Set a new valid [State] as the latest in the [FlashJournal].
+async fn set_status<B: Board>(board: &mut B, state: &mut State, status: Status) {
+    *state = state.with_status(status);
+    if let Err(_e) = board.journal().set::<JOURNAL_BUFFER_SIZE>(state).await {
+        panic!("Failed to update state"); // TODO print e, but requirements for defmt are in the way.
+    }
 }
 
 #[embassy_executor::main]
@@ -54,16 +102,61 @@ async fn main(_spawner: Spawner) -> ! {
     // Load descriptors, if flashed at all.
     let descriptors = descriptors::load();
 
-    let board = init().await;
+    let mut board = init().await;
+    let state = board.journal().get();
 
-    let active_slot = 1; // TODO
+    // Fetch state or set initial state.
+    let mut state: State = match state {
+        Some(state) => *state,
+        None => {
+            let slot = unwrap!(Slot::try_from(0));
+            warn!(
+                "Initial bootup and no state was loaded into the journal, attempting {:?}",
+                slot
+            );
+            State::new(Status::Initial, slot, slot)
+        }
+    };
 
-    let active_app_descriptor = descriptors.get_app_at_slot(active_slot);
+    // Determine our intended slot to boot.
+    let intent = match state.status() {
+        Status::Initial => {
+            // Mark the status to [Attempting], so that the app can mark the status to [Confirmed].
+            set_status(&mut board, &mut state, Status::Attempting).await;
+            BootIntent::Target
+        }
+        Status::Attempting => {
+            // When the bootloader starts with the state [Attempting],
+            // it implies that an attempt was made to start the application in the slot,
+            // but the application failed to mark the slot as [Confirmed].
+            set_status(&mut board, &mut state, Status::Failed).await;
+            BootIntent::Backup
+        }
+        Status::Failed => BootIntent::Backup,
+        Status::Confirmed => BootIntent::Target,
+    };
 
-    info!("Bootloader: Performing image copy to execution location.");
-    // copy_image(&active_app_descriptor);
+    // Translate the abstract intention to a concrete slot.
+    let slot = match intent {
+        BootIntent::Target => state.target(),
+        BootIntent::Backup => state.backup(),
+    };
 
-    // branch to location as described by descriptor
-    // unsafe { bootload::boot_application(active_app_descriptor.execution_address as *const u32) };
-    loop {}
+    info!("Attempting to boot {:?} in {:?}", intent, slot);
+    let error = attempt_slot(slot, &mut board, &descriptors).await; // If this function returns, it implies that the boot has failed.
+    warn!("Failed to boot {:?} in {:?} because {:?}", intent, slot, error);
+
+    if slot != state.backup() {
+        // There exists a separate backup slot.
+        // That implies that we were in either [Initial] or [Confirmed].
+        // But the target [Slot] did not work, so will give up on it forever.
+        set_status(&mut board, &mut state, Status::Failed).await;
+
+        info!("Attempting to boot backup in {:?}", slot);
+        let error = attempt_slot(state.backup(), &mut board, &descriptors).await; // If this function returns, it implies that the boot has failed.
+        warn!("Failed to boot backup in {:?} because {:?}", slot, error);
+    }
+
+    error!("No candidates booted successfully, giving up...");
+    board.abort()
 }
